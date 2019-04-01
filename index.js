2b0b46b2b11d40fad81fef8e4ac5c94589febcfe
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-04-01 16:26:59 -0400

    create a global restricted route per the stretch assignment

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 const db = knex(knexConfig.development);
 
+const RestrictedRouter = require('./routers/RestrictedRouter.js');
+
 server.use(express.json());
 
+
+        //base line routes...
 server.get('/', (req,res) => {
     res.send('welcome to auth')
 })
-
-
 server.post('/api/register', (req,res) => {
     //Creates a user using the information sent inside the body of the request. Hash the password before saving the user to the database.
     let user = req.body;
@@ -25,7 +27,6 @@ server.post('/api/register', (req,res) => {
         .then(response => res.status(201).json(response))
         .catch(err => res.status(500).json(err));
 })
-
 server.post('/api/login', (req,res) => {
     //Use the credentials sent inside the body to authenticate the user. On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. If login fails, respond with the correct status code and the message: 'You shall not pass!'
 
@@ -41,7 +42,6 @@ server.post('/api/login', (req,res) => {
         }).catch(err => res.status(500).json({message: 'there was an error retrieving your account...'}));
 
 })
-
 server.get('/api/users', restricted, (req,res) => {
     //If the user is logged in, respond with an array of all the users contained in the database. 
     //If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
@@ -50,28 +50,50 @@ server.get('/api/users', restricted, (req,res) => {
         .catch(err => res.status(500).json(err));
 
 })
-
 function restricted(req,res,next){
     let { username, password } = req.headers;
 
     if(username && password){
   
-    db('users')
-        .where({ username: username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-            next()
-            } else {
-            res.status(401).json({ message: 'YOU SHALL NOT PAAAASSSSSSSs' });
-            }
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
-    }
+        db('users')
+            .where({ username: username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                next()
+                } else {
+                res.status(401).json({ message: 'YOU SHALL NOT PAAAASSSSSSSs' });
+                }
+            })
+            .catch(error => {
+                res.status(500).json(error);
+            });
+        }
     else{
         res.status(401).json({message: "Please provied credentials"})
+    }
+}
+
+
+        //restricted routes
+server.use('/api/restriced', retrictRoute, RestrictedRouter);
+
+function retrictRoute(req,res,next){
+    const { username, password } = req.headers
+
+    if(username && password){
+        db('users')
+            .where({ username })
+            .first()
+            .then(user => {
+                if(user && bcrypt.compareSync(password, user.password)){
+                    next();
+                }else{
+                    res.status(401).json({ message: 'you arent allowed here...' });
+                }
+            })
+    }else{
+        res.status(500).json({errorMessage: "you arent logged in!"})
     }
 }
 
